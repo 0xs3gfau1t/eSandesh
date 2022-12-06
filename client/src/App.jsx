7d7277e51ddd9a36a109271d7c9a26c51d0e2aa5
Sandesh GC <gcsandesh01@gmail.com>
2022-12-06 18:58:04 +0545

    initiated navbar with basic route and folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Politics from "./pages/Politics";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1 className=" font-primary text-3xl">eSandesh</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/politics" element={<Politics />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import siteLogo from "../images/icons/eSandesh-logo--dark.png";
 
 export default function Header() {
-  return <div className=" font-primary">Header</div>;
+  return (
+    <div className="border border-red-700 font-primary flex justify-between items-center">
+      <img
+        src={siteLogo}
+        className="w-40"
+        alt="logo for eSandesh news portal"
+      ></img>
+      <nav>
+        <ul className="list-none flex justify-between gap-8">
+          <Link to={""}>
+            <li>Home</li>
+          </Link>
+          <Link to={""}>
+            <li>Trending</li>
+          </Link>
+          <Link to={""}>
+            <li>Global</li>
+          </Link>
+          <Link to={""}>
+            <li>Business</li>
+          </Link>
+          <Link to={"/politics"}>
+            <li>Politics</li>
+          </Link>
+          <Link to={""}>
+            <li>Finance</li>
+          </Link>
+          <Link to={""}>
+            <li>Economy</li>
+          </Link>
+          <Link to={""}>
+            <li>Sports</li>
+          </Link>
+          <Link to={""}>
+            <li>Entertainment</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
