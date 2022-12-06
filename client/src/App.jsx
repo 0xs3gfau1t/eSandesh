29dd340806dc1b90b6092c16da2d735bd641c4b1
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-06 17:08:40 +0545

    Setup tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css"
 
 function App() {
-	return (
-		<div className="App">
-			<h1>NEWS PORTAL</h1>
-		</div>
-	)
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
