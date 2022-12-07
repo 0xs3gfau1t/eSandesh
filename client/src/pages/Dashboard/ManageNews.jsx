afc183aa14df38f2ee42f27981009007f8dc4b92
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-07 18:02:58 +0545

    List all news in admin dashboard

--- client/src/pages/Dashboard/ManageNews.jsx
+++ client/src/pages/Dashboard/ManageNews.jsx
@@ -3,15 +3,15 @@ import { useDispatch, useSelector } from "react-redux"
 import { BsFillPlusSquareFill } from "react-icons/bs"
 import { Link } from "react-router-dom"
 import { listNews } from "../../redux/actions/dashNews"
+import { useState } from "react"
 
 const ManageNews = () => {
-	const news = useSelector(state => state.dashNews)
+	const news = useSelector(state => state.dashNews.newsList)
 	const dispatch = useDispatch()
-
+	let [page, setPage] = useState(0)
 	useEffect(() => {
-		console.log("Hemlo")
-		dispatch(listNews())
-	}, [])
+		dispatch(listNews(page))
+	}, [page])
 
 	return (
 		<>
@@ -27,57 +27,50 @@ const ManageNews = () => {
 					<div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
 						<div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
 							<div className="overflow-hidden">
-								<table className="min-w-full table-auto">
+								<table className="newsListTable min-w-full table-auto">
 									<thead className="border-b">
 										<tr>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												#
-											</th>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Title
-											</th>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Date Created
-											</th>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Hits
-											</th>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Categories
-											</th>
+											<th scope="col">#</th>
+											<th scope="col">Title</th>
+											<th scope="col">Date Modified</th>
+											<th scope="col">Hits</th>
+											<th scope="col">Categories</th>
+											<th scope="col">Actions</th>
 										</tr>
 									</thead>
 									<tbody>
-										<tr className="border-b">
-											<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-												1
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												Mark hbjhbfgdhfjgb ghdfhgdkfh
-												hgdfghdkj gjfdghdkjf
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												Otto
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												@mdo
-											</td>
-										</tr>
+										{news[page] &&
+											Object.keys(news[page]).map(nws => {
+												let khabar = news[page][nws]
+												return (
+													<tr
+														key={nws}
+														className="border-b"
+													>
+														<td>
+															{parseInt(nws) + 1}
+														</td>
+														<td>
+															{khabar.title
+																? khabar.title
+																: ""}
+														</td>
+														<td>
+															{new Date(
+																khabar.updatedAt
+															).toLocaleDateString()}
+														</td>
+														<td>{khabar.hits}</td>
+														<td>
+															{khabar.category
+																? khabar.category.join(
+																		","
+																  )
+																: ""}
+														</td>
+													</tr>
+												)
+											})}
 									</tbody>
 								</table>
 							</div>
--- client/src/redux/reducers/dashNews.js
+++ client/src/redux/reducers/dashNews.js
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { listNews } from "../actions/dashNews"
 
 const initialState = {
-	newsList: [],
+	newsList: {},
 }
 
 const miscSlice = createSlice({
 	name: "dashNews",
 	initialState,
 	reducers: {},
+
+	extraReducers: builder => {
+		builder.addCase(listNews.fulfilled, (state, { payload }) => {
+			if (payload.success && payload.data) {
+				state.newsList[payload.page] = payload.data
+			}
+		})
+	},
 })
 
 export default miscSlice.reducer
