5e71b051a26adadb728c0ab5cc1980c54003db02
needl3 <57314527+needl3@users.noreply.github.com>
2022-12-07 22:28:53 +0545

    Add auth middleware in comment route

--- server/routes/comment/index.js
+++ server/routes/comment/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const withAuth = require('../../middlewares/withAuth')
+
 const addComment = require('./addComment')
 const delComment = require('./delComment')
 const editComment = require('./editComment')
 const getComments = require('./getComments')
 
 router.get('/', getComments)
-router.post('/', addComment)
-router.delete('/', delComment)
-router.patch('/', editComment)
+router.post('/', withAuth, addComment)
+router.delete('/', withAuth, delComment)
+router.patch('/', withAuth, editComment)
 
 module.exports = router
