135ab228a19a55064404951b83ca7cab0f2aa667
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-12-07 21:54:23 +0545

    list articles by year and month

--- server/routes/article/index.js
+++ server/routes/article/index.js
@@ -9,11 +9,10 @@ const editArticle = require('./editArticle')
 const getArticle = require('./getArticle')
 const listArticle = require('./listArticle')
 
+router.get('/list*', listArticle)
 router.get('/:year/:month/:slug', getArticle)
 router.post('/', withAuth, addArticle)
 router.delete('/', withAuth, delArticle)
 router.patch('/', withAuth, editArticle)
-
-router.get('/list/', listArticle)
 
 module.exports = router
