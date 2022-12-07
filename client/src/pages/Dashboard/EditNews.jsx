97957ff84a4221958267d187726a50baec43a960
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-07 14:04:34 +0545

    Add new article from client

--- client/src/pages/Dashboard/EditNews.jsx
+++ client/src/pages/Dashboard/EditNews.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+import { useDispatch } from "react-redux"
 import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
 
 import EditorToolbar, { modules, formats } from "./EditorTools"
-import "react-quill/dist/quill.snow.css"
+import { FormText } from "../../components/common"
+import { addNews } from "../../redux/actions/dashNews"
 
 const initState = {
 	title: "",
@@ -12,20 +15,42 @@ const initState = {
 
 const EditNews = () => {
 	const [news, setNews] = useState("")
-	const [property, setProp] = useState()
+	const [property, setProp] = useState(initState)
 
+	const dispatch = useDispatch()
+
+	const handleChange = e => {
+		setProp({ ...property, [e.target.name]: e.target.value })
+	}
+
+	const onPublish = e => {
+		dispatch(addNews({ content: news, ...property }))
+	}
 	return (
-		<div className="ml-4">
-			<h1>Edit News</h1>
-			<EditorToolbar />
-			<ReactQuill
-				theme="snow"
-				value={news}
-				onChange={setNews}
-				placeholder={"What's hot?..."}
-				modules={modules}
-				formats={formats}
-			/>
+		<div className="ml-4 w-4/5 flex gap-4">
+			<div>
+				<FormText
+					type="text"
+					name="title"
+					value={property.title}
+					labelText="Title"
+					handleChange={handleChange}
+				/>
+				<EditorToolbar />
+				<ReactQuill
+					theme="snow"
+					value={news}
+					onChange={setNews}
+					placeholder={"What's hot?..."}
+					modules={modules}
+					formats={formats}
+				/>
+			</div>
+			<div>
+				<button className="" onClick={onPublish}>
+					Publish
+				</button>
+			</div>
 		</div>
 	)
 }
