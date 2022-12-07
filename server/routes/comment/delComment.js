0e20447a2602572f462969f2ee52aaec970366b1
needl3 <57314527+needl3@users.noreply.github.com>
2022-12-07 11:44:18 +0545

    Comment routes
    - Basic CRUD for comments
    - Cleaning children not implemented yet
    - Minor changes in models

--- server/routes/comment/delComment.js
+++ server/routes/comment/delComment.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const commentModel = require('../../model/comment')
+
 /**
  * @param {express.Request} req
  * @param {express.Response} res
  * @return {void}
  */
 const delComment = (req, res) => {
-    const { id } = req.query
+	const { commentId } = req.query
+	const { userId } = req.auth
+
+	commentModel.findOneAndDelete(
+		{
+			_id: commentId,
+			user: userId,
+		},
+		(e, d) => {
+			if (e) return res.json({ error: 'Comment Not Found' })
+			res.json({ message: 'success', comment: d })
+		}
+	)
 }
 
 module.exports = delComment
--- server/routes/comment/getComments.js
+++ server/routes/comment/getComments.js
@@ -1,11 +1,42 @@
-const express = require('express')
 /**
  * @param {express.Request} req
  * @param {express.Response} res
  * @return {void}
  */
-const getComments = (req, res) => {
-    const { articleId: id, page = 0 } = req.query
+
+
+const express = require('express')
+const commentModel = require('../../model/comment')
+
+const getComments = async (req, res) => {
+	const { articleId, userId, page, items } = req.query
+
+	if (articleId) {
+		try {
+			const comments = await commentModel
+				.find({ article: articleId })
+				.skip(page || 0)
+				.limit(items || 10)
+
+			res.json({ message: 'success', comments: comments })
+		} catch (e) {
+			console.log(e)
+			res.json({ error: 'Something went wrong' })
+		}
+	}
+
+	if (userId) {
+		try {
+			const comments = await commentModel
+				.find({ user: userId })
+				.skip(page || 0)
+				.limit(items || 10)
+			res.json({ message: 'success', comments: comments })
+		} catch (e) {
+			console.log(e)
+			res.status(500).json({ error: 'Something went wrong' })
+		}
+	}
 }
 
 module.exports = getComments
