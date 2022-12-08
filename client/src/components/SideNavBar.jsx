20dae6dc1b20c341322c880f5cfee21941a5270d
Sandesh GC <gcsandesh01@gmail.com>
2022-12-08 13:10:41 +0545

    fixed footer coming up

--- client/src/components/SideNavBar.jsx
+++ client/src/components/SideNavBar.jsx
@@ -1,34 +1,45 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function sideNavBar() {
-	const categories = [
-		["Admin", , "admin"], //remove admin from this list and add other category
-		["राजनीति", "politics"],
-		["विश्व", , "global"],
-		["विजनेस", "business"],
-		["अर्थ/वाणिज्य", "finance"],
-		["खेलकुद", "sports"],
-		["मनोरञ्जन", "entertainment"],
-	]
-	const menuItems = categories.map(eachCat => {
-		return (
-			<li
-				className="hover:font-bold duration-200 hover:text-darkblue my-2"
-				key={eachCat[0]}
-			>
-				<Link to={`/category/${eachCat[1]}`}>{eachCat[0]}</Link>
-			</li>
-		)
-	})
-	return (
-		<div>
-			<nav className="p-4 border-r-4 border-r-darkblue">
-				<h2 className="font-primary font-bold text-2xl">Category</h2>
-				<ul className="list-none flex flex-col p-4 justify-between">
-					{menuItems}
-				</ul>
-			</nav>
-		</div>
-	)
+export default function SideNavBar(activeCategory) {
+  const categories = [
+    // ["Admin", , "admin"], //remove admin from this list and add other category
+    ["राजनीति", "politics"],
+    ["विश्व", , "global"],
+    ["विजनेस", "business"],
+    ["अर्थ/वाणिज्य", "finance"],
+    ["खेलकुद", "sports"],
+    ["मनोरञ्जन", "entertainment"],
+  ];
+  const menuItems = categories.map((eachCat) => {
+    if (eachCat[1] === activeCategory) {
+      return (
+        <li
+          className="hover:font-bold duration-200 text-darkblue hover:text-darkblue my-2"
+          key={eachCat[0]}
+        >
+          <Link to={`/category/${eachCat[1]}`}>{eachCat[0]}</Link>
+        </li>
+      );
+    } else {
+      return (
+        <li
+          className="hover:font-bold duration-200 hover:text-darkblue my-2"
+          key={eachCat[0]}
+        >
+          <Link to={`/category/${eachCat[1]}`}>{eachCat[0]}</Link>
+        </li>
+      );
+    }
+  });
+  return (
+    <div>
+      <nav className="p-4 border-r-4 border-r-darkblue">
+        <h2 className="font-primary font-bold text-2xl">Category</h2>
+        <ul className="list-none flex flex-col p-4 justify-between">
+          {menuItems}
+        </ul>
+      </nav>
+    </div>
+  );
 }
