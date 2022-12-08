555447fcd92b2726881dc17461d169b625d6c256
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-08 12:43:39 +0545

    Fix error in categor list side nav

--- client/src/components/SideNavBar.jsx
+++ client/src/components/SideNavBar.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom"
 
 export default function sideNavBar() {
 	const categories = [
-		"Admin", //remove admin from this list and add other category
-		"राजनीति",
-		"विश्व",
-		"विजनेस",
-		"अर्थ/वाणिज्य",
-		"खेलकुद",
-		"मनोरञ्जन",
+		["Admin", , "admin"], //remove admin from this list and add other category
+		["राजनीति", "politics"],
+		["विश्व", , "global"],
+		["विजनेस", "business"],
+		["अर्थ/वाणिज्य", "finance"],
+		["खेलकुद", "sports"],
+		["मनोरञ्जन", "entertainment"],
 	]
-	const menuItems = categories.map(eachCat => (
-		<li
-			className="hover:font-bold duration-200 hover:text-darkblue my-2"
-			key={eachCat.toLowerCase()}
-		>
-			<Link to={`/category/${eachCat.toLowerCase()}`}>{eachCat}</Link>
-		</li>
-	))
+	const menuItems = categories.map(eachCat => {
+		return (
+			<li
+				className="hover:font-bold duration-200 hover:text-darkblue my-2"
+				key={eachCat[0]}
+			>
+				<Link to={`/category/${eachCat[1]}`}>{eachCat[0]}</Link>
+			</li>
+		)
+	})
 	return (
 		<div>
 			<nav className="p-4 border-r-4 border-r-darkblue">
