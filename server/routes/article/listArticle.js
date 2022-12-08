da4a3c42368f3c36415951e427564021c7cc599a
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-12-08 21:42:38 +0545

    if not logged in use cookies

--- server/routes/article/listArticle.js
+++ server/routes/article/listArticle.js
@@ -35,27 +35,38 @@ const listArticle = async (req, res) => {
 
         // If user is logged in sort the list based on their history
         // Maybe i could've used aggregations but this seemed way easier
+        var user, isLoggedIn
         if (req.session?.user && !req.session.user?.roles?.isRoot) {
-            const user = await userModel.findOne(
+            user = await userModel.findOne(
                 { _id: req.session.user.id },
                 { history: true }
             )
-            articles.sort((a, b) => {
-                var scoreA = a.category.reduce((accum, value) => {
-                    return accum + (user.history?.get(value) || 0)
-                }, 0)
-
-                var scoreB = b.category.reduce((accum, value) => {
-                    return accum + (user.history?.get(value) || 0)
-                }, 0)
-
-                const diff = scoreB - scoreA
-                if (diff) return diff
-
-                // If both scored same points then sort based on hits
-                return b.hits - a.hits
-            })
+            isLoggedIn = true
+        } else {
+            user = req.cookies?.user
+            if (user) user = JSON.parse(user)
+            else user = { history: {} }
+            isLoggedIn = false
         }
+        articles.sort((a, b) => {
+            var scoreA = a.category.reduce((accum, value) => {
+                return accum + isLoggedIn
+                    ? user.history?.get(value) || 0
+                    : user.history[value] || 0
+            }, 0)
+
+            var scoreB = b.category.reduce((accum, value) => {
+                return accum + isLoggedIn
+                    ? user.history?.get(value) || 0
+                    : user.history[value] || 0
+            }, 0)
+
+            const diff = scoreB - scoreA
+            if (diff) return diff
+
+            // If both scored same points then sort based on hits
+            return b.hits - a.hits
+        })
 
         return res.status(200).json(articles)
     } catch (err) {
