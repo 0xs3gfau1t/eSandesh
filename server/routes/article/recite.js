77a343aace3df07514b47e93dea8999a5cbd448e
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-12-08 20:43:35 +0545

    optimize recite route

--- server/routes/article/recite.js
+++ server/routes/article/recite.js
@@ -1,4 +1,4 @@
-const article = require('../../model/article')
+const articleModel = require('../../model/article')
 const path = require('path')
 const audioAdFolder = path.resolve(__dirname, '../../assets/')
 const fs = require('fs')
@@ -9,64 +9,34 @@ const fs = require('fs')
  * @return {void}
  */
 
-module.exports = (_req, _res) => {
-    const { articleId } = _req.body
+module.exports = async (req, res) => {
+    const { id } = req.query
 
-    article.findOne({ _id: articleId }, (e, d) => {
-        if (e) return _res.json({ error: 'No such article found' })
+    try {
+        const article = await articleModel.findOne({ _id: id })
+        if (!article) return res.json({ error: 'No such article found' })
 
-        const content = d.title
-        const https = require('follow-redirects').https
-        const options = {
-            method: 'POST',
-            hostname: 'app.micmonster.com',
-            path: '/restapi/create',
-            headers: {},
-            maxRedirects: 20,
-        }
-
-        const req = https.request(options, function (res) {
-            const chunks = []
-
-            res.on('data', function (chunk) {
-                chunks.push(chunk)
-            })
-            res.on('end', function (chunk) {
-                const body = Buffer.concat(chunks)
-                try {
-                    fs.readFile(audioAdFolder + '/ad.mp3', (e, ad) => {
-                        if (e) throw Error('Cannot read ad file')
-
-                        const decodedBody = Buffer.from(
-                            body.toString(),
-                            'base64'
-                        )
-                        const concatenated = Buffer.concat(
-                            [ad, decodedBody],
-                            ad.length + decodedBody.length
-                        )
-                        return _res.send(concatenated)
-                    })
-                } catch (e) {
-                    _res.json({
-                        error: 'Something went wrong while appending ad',
-                    })
-                }
-            })
-
-            res.on('error', function (error) {
-                _res.json({ error: 'Error while converting to audio' })
-            })
-        })
-
-        const postData = `------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="locale"\r\n\r\nne-NP\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="content"\r\n\r\n<voice name="ne-NP-SagarNeural">${content}</voice>\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="ip"\r\n\r\n202.51.80.138\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--`
-
-        req.setHeader(
-            'content-type',
-            'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        const body = new FormData()
+        body.append('locale', 'ne-NP')
+        body.append(
+            'content',
+            '<voice name="ne-NP-SagarNeural">' + article.title + '</voice>'
         )
+        body.append('ip', '194.74.255.198')
 
-        req.write(postData)
-        req.end()
-    })
+        const sound = await fetch('https://app.micmonster.com/restapi/create', {
+            method: 'POST',
+            body,
+        })
+            .then(res => res.text())
+            .then(base64Str => new Buffer.from(base64Str, 'base64'))
+
+        const ad = fs.readFileSync(audioAdFolder + '/ad.mp3')
+        const concatenated = Buffer.concat([ad, sound])
+
+        return res.send(concatenated)
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: 'Something went wrong.' })
+    }
 }
