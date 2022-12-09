286122643a42735c41b0c706202488461df2c5b8
SandeshGC <gcsandesh01@gmail.com>
2022-12-09 20:14:08 +0545

    fix layout

--- client/src/pages/Home/Landing.jsx
+++ client/src/pages/Home/Landing.jsx
@@ -1,57 +1,57 @@
-import HomeHero from "./HomeHero"
-import RecentNews from "./RecentNews"
-import SideScrollNewsSection from "./SideScrollNewsSection"
-import { RectAds } from "../../components/common"
-import EachCategoryPreview from "./EachCategoryPreview"
-import SportsHighlights from "./SportsHighlights"
+import HomeHero from "./HomeHero";
+import RecentNews from "./RecentNews";
+import SideScrollNewsSection from "./SideScrollNewsSection";
+import { RectAds } from "../../components/common";
+import EachCategoryPreview from "./EachCategoryPreview";
+import SportsHighlights from "./SportsHighlights";
 
 const Landing = () => {
-	return (
-		<div>
-			<div className=" w-full sm:w-3/5 px-4">
-				<div className="flex">
-					<div className="w-full sm:w-2/3">
-						<HomeHero />
-					</div>
-					<div className="w-1/3 sm:block hidden">
-						<RecentNews />
-					</div>
-				</div>
-				<h1 className="font-semibold text-2xl">Hottest Topics</h1>
-				<SideScrollNewsSection category={"hot"} />
-				<RectAds
-					type={
-						"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-					}
-				/>
-				<div className="mb-10 pb-4">
-					{/* middle scrolling news under the hero section */}
-					<EachCategoryPreview category="health" />
-					<RectAds
-						type={
-							"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-						}
-					/>
-				</div>
-				<div className="mb-10 pb-4">
-					{/* middle scrolling news under the hero section */}
-					<EachCategoryPreview category="technology" />
-					<RectAds
-						type={
-							"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-						}
-					/>
-				</div>
-				{/* other category previews go here */}I think other category
-				previews should be placed here ok
-				{/* end of categories preview / middle scrolling section ends here */}
-			</div>
-			{/* end of outlet */}
-			<div className="sm:w-1/5 sm:block hidden">
-				<SportsHighlights />
-			</div>
-		</div>
-	)
-}
+  return (
+    <>
+      <div className=" w-full sm:w-3/5 px-4">
+        <div className="flex">
+          <div className="w-full sm:w-2/3">
+            <HomeHero />
+          </div>
+          <div className="w-1/3 sm:block hidden">
+            <RecentNews />
+          </div>
+        </div>
+        <h1 className="font-semibold text-2xl">Hottest Topics</h1>
+        <SideScrollNewsSection category={"hot"} />
+        <RectAds
+          type={
+            "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+          }
+        />
+        <div className="mb-10 pb-4">
+          {/* middle scrolling news under the hero section */}
+          <EachCategoryPreview category="health" />
+          <RectAds
+            type={
+              "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+            }
+          />
+        </div>
+        <div className="mb-10 pb-4">
+          {/* middle scrolling news under the hero section */}
+          <EachCategoryPreview category="technology" />
+          <RectAds
+            type={
+              "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+            }
+          />
+        </div>
+        {/* other category previews go here */}I think other category previews
+        should be placed here ok
+        {/* end of categories preview / middle scrolling section ends here */}
+      </div>
+      {/* end of outlet */}
+      <div className="sm:w-1/5 sm:block hidden">
+        <SportsHighlights />
+      </div>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
