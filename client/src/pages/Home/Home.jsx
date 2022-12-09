b958dde8ea1573f9abdcdb91aca7a0fcbbd8fda8
SandeshGC <gcsandesh01@gmail.com>
2022-12-09 15:50:58 +0545

    added health section below hero in home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import { FaHandPointRight } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 import { Footer, Header, SideNavBar } from "../../components";
 import {
-  ArticlePreviewMd,
   ArticlePreviewSm,
   LikeSaveShare,
   RectAds,
@@ -101,21 +102,28 @@ export default function Home(match) {
               "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
             }
           />
-
-          <div>
+          <div className="mb-10 pb-4">
             {/* middle scrolling news under the hero section */}
-            <h1 className="text-4xl font-bold">Trending in Health</h1>I think
-            other category previews should be placed here ok
-            <div className="flex">
+            <h1 className="text-4xl font-bold leading-loose">
+              Trending in Health
+            </h1>
+            <div className="flex mb-10 pb-4">
               <div className="w-2/3">
                 <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" />
                 <div className="flex items-end justify-between">
-                  <h1 className="text-3xl">Scientists have found Covid- 19</h1>
-                  <span>2020, March- 12</span>
-                  <LikeSaveShare
-                    // articleId={}
-                    likes={"100k"}
-                  />
+                  <h1 className="text-3xl border border-blue w-3/5">
+                    Scientists have found Covid- 19
+                  </h1>
+                  <div className="flex items-center border-red border w-2/5">
+                    <span className="text-xs text-slate-400 w-1/2">
+                      2020 March- 12
+                    </span>
+                    <LikeSaveShare
+                      // articleId={}
+                      likes={"100k"}
+                      className="w-1/2"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="px-4 py-2">
@@ -124,21 +132,96 @@ export default function Home(match) {
                 </h2>
                 <ul className="py-2">
                   <li className="flex">
-                    <FaHandPointRight className="mt-1 mr-2" />{" "}
+                    <FaHandPointRight className="mt-1 mr-2" />
                     <span className="hover:text-rose-700 font-semibold transition-colors">
-                      Walter White gets Cancer, from Staying in the sun
+                      <Link to={"articleURL"}>
+                        Walter White gets Cancer, from Staying in the sun
+                      </Link>
                     </span>
                   </li>
                   <li className="flex">
                     <FaHandPointRight className="mt-1 mr-2" />
                     <span className="hover:text-rose-700 font-semibold transition-colors">
-                      Drink Warm Water
+                      <Link to={"articleURL"}>Drink Warm Water</Link>
                     </span>
                   </li>
                 </ul>
               </div>
             </div>
+            <div>
+              <h1 className="font-semibold text-2xl leading-loose">
+                More from Health
+              </h1>
+              <div className="flex items-start justify-between gap-4 overflow-x-scroll">
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+                <ArticlePreviewSm
+                  title={"Maanis harayeko suchana"}
+                  summary={
+                    "26/ F, Suntali Pandey, Call: 9812347599, Last seen at WRC gate"
+                  }
+                  imgUrl={
+                    "http://kunjanaghimire.com/storage/images/kunjana-hero-2.png"
+                  }
+                  articleUrl={"yo artikle ko link hai ta ya"}
+                />
+              </div>
+            </div>
+            <RectAds
+              type={
+                "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+              }
+            />
           </div>
+          I think other category previews should be placed here ok
         </div>
 
         {/* end of middle news/ scrolling section */}
