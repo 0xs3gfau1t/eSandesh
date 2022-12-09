6d045d5c2e159ef62f76b5d6fdaa5b744b555f27
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-09 19:57:00 +0545

    Fix homepage layout issues

--- client/src/pages/Home/SingleNews.jsx
+++ client/src/pages/Home/SingleNews.jsx
@@ -1,28 +1,38 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
 
-import { getSingleNews } from "../../redux/actions/publicNews";
+import { SqAds } from "../../components/common"
+import { getSingleNews } from "../../redux/actions/publicNews"
 
 const SingleNews = () => {
-  const params = useParams();
-  const news = useSelector((state) => state.news.singleNews);
-  const dispatch = useDispatch();
+	const params = useParams()
+	const news = useSelector(state => state.news.singleNews)
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getSingleNews(params));
-  }, []);
-  return (
-    <div className="news-content my-2 mx-8 w-3/5">
-      <h1 className="text-xl m-4 font-bold">{news ? news.title : ""}</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: news ? news.content : "Fetching",
-        }}
-        className=""
-      />
-    </div>
-  );
-};
+	useEffect(() => {
+		dispatch(getSingleNews(params))
+	}, [])
+	return (
+		<div className="flex justify-between container gap-4">
+			<div className="news-content ml-4 mb-4">
+				<h1 className="text-xl m-4 font-bold">
+					{news ? news.title : ""}
+				</h1>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: news ? news.content : "Fetching",
+					}}
+					className=""
+				/>
+			</div>
+			<div className="hidden sm:block px-4">
+				{/* ads go here */}
+				<SqAds />
+				<SqAds />
+			</div>
+		</div>
+	)
+}
 
-export default SingleNews;
+export default SingleNews
