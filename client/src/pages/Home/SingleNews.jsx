de45e80a83959aba54f12793ab76534520daa2fb
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-10 15:00:17 +0545

    minor styles changes

--- client/src/pages/Home/SingleNews.jsx
+++ client/src/pages/Home/SingleNews.jsx
@@ -1,85 +1,59 @@
-import { useEffect } from "react";
-import { FaPause, FaPlay } from "react-icons/fa";
-import { ImVolumeDecrease, ImVolumeIncrease } from "react-icons/Im";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
 
-import { SqAds } from "../../components/common";
-import { getSingleNews, getNewsAudio } from "../../redux/actions/publicNews";
+import { SqAds } from "../../components/common"
+import { getSingleNews, getNewsAudio } from "../../redux/actions/publicNews"
 
 const SingleNews = () => {
-  const params = useParams();
-  const news = useSelector((state) => state.news.singleNews);
-  const audio = useSelector((state) => state.news.audio);
+	const params = useParams()
+	const news = useSelector(state => state.news.singleNews)
+	const audio = useSelector(state => state.news.audio)
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getSingleNews(params));
-  }, []);
-  return (
-    <div className="flex justify-between container gap-4">
-      <div className="news-content ml-4 mb-10 w-full">
-        <h1 className="text-xl m-4 font-bold">{news ? news.title : ""}</h1>
-        <div className="my-4 w-min mx-auto">
-          {audio ? (
-            <audio controls id="audioPlayer" className="bg-gray-100 rounded-md">
-              <source
-                src={news ? `data:audio;base64,${audio}` : "#"}
-                type="audio/mpeg"
-                className=" bg-blue"
-              />
-              Your browser does not support the audio element.
-            </audio>
-          ) : (
-            <h1>Loading news audio...</h1>
-          )}
-          {/* <div className="flex items-center">
-            <button
-              onClick={() => {
-                document.getElementById("audioPlayer").play();
-              }}
-            >
-              <FaPlay />
-            </button>
-            <button
-              onClick={() => {
-                document.getElementById("audioPlayer").pause();
-              }}
-            >
-              <FaPause />
-            </button>
-            <button
-              onClick={() => {
-                document.getElementById("audioPlayer").volume -= 0.2;
-              }}
-            >
-              <ImVolumeDecrease />
-            </button>
-            <button
-              onClick={() => {
-                document.getElementById("audioPlayer").volume += 0.2;
-              }}
-            >
-              <ImVolumeIncrease />
-            </button>
-          </div> */}
-        </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: news ? news.content : "Fetching",
-          }}
-          // className="flex items-center justify-center w-full"
-        />
-      </div>
-      {/* right column */}
-      <div className="hidden sm:block px-4">
-        {/* ads go here */}
-        <SqAds />
-        <SqAds />
-      </div>
-    </div>
-  );
-};
+	useEffect(() => {
+		dispatch(getSingleNews(params))
+	}, [])
+	return (
+		<div className="flex justify-between container gap-4">
+			<div className="news-content ml-4 mb-10 w-full">
+				<h1 className="text-xl m-4 font-bold">
+					{news ? news.title : ""}
+				</h1>
+				<div className="my-4 w-min mx-auto">
+					{audio ? (
+						<audio
+							controls
+							id="audioPlayer"
+							className="bg-gray-100 rounded-md"
+						>
+							<source
+								src={news ? `data:audio;base64,${audio}` : "#"}
+								type="audio/mpeg"
+								className=" bg-blue"
+							/>
+							Your browser does not support the audio element.
+						</audio>
+					) : (
+						<h1 className="w-max">Loading news audio...</h1>
+					)}
+				</div>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: news ? news.content : "Fetching",
+					}}
+					// className="flex items-center justify-center w-full"
+				/>
+			</div>
+			{/* right column */}
+			<div className="hidden sm:block px-4">
+				{/* ads go here */}
+				<SqAds />
+				<SqAds />
+			</div>
+		</div>
+	)
+}
 
-export default SingleNews;
+export default SingleNews
