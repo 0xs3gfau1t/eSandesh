e9c3301de111c586054e941ea4996153543fbb61
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-10 23:27:24 +0545

    Focus mode option added

--- client/src/components/LeftSideBar.jsx
+++ client/src/components/LeftSideBar.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import NavBar from "./NavBar";
-import TopCritics from "./Critics/TopCritics";
+import React from "react"
+import NavBar from "./NavBar"
+import TopCritics from "./Critics/TopCritics"
 
 function LeftSideBar() {
-  return (
-    <div className="flex flex-col">
-      <NavBar />
-      <TopCritics />
-    </div>
-  );
+	return (
+		<div className="flex flex-col">
+			<NavBar />
+			<TopCritics />
+		</div>
+	)
 }
 
-export default LeftSideBar;
+export default LeftSideBar
--- client/src/pages/Home/Landing.jsx
+++ client/src/pages/Home/Landing.jsx
@@ -1,68 +1,68 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 
-import HomeHero from "./HomeHero";
-import RecentNews from "./RecentNews";
-import SideScrollNewsSection from "./SideScrollNewsSection";
-import { RectAds } from "../../components/common";
-import EachCategoryPreview from "./EachCategoryPreview";
-import { getHotNews } from "../../redux/actions/publicNews";
+import HomeHero from "./HomeHero"
+import RecentNews from "./RecentNews"
+import SideScrollNewsSection from "./SideScrollNewsSection"
+import { RectAds } from "../../components/common"
+import EachCategoryPreview from "./EachCategoryPreview"
+import { getHotNews } from "../../redux/actions/publicNews"
 
 const Landing = () => {
-  const hot = useSelector((state) => state.news.hotNews);
-  const dispatch = useDispatch();
+	const hot = useSelector(state => state.news.hotNews)
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getHotNews(0));
-  }, []);
+	useEffect(() => {
+		dispatch(getHotNews(0))
+	}, [])
 
-  return (
-    <>
-      <div className=" w-full sm:w-3/5 px-4">
-        {hot && (
-          <div className="flex gap-6">
-            <div className="w-full sm:w-2/3">
-              <HomeHero data={hot[0]} />
-            </div>
-            <div className="w-1/3 sm:block hidden">
-              <h2 className=" text-2xl font-semibold leading-loose">
-                Recent News
-              </h2>
-              <RecentNews data={hot.slice(1)} />
-            </div>
-          </div>
-        )}
-        <h1 className="font-semibold text-2xl">Hottest Topics</h1>
-        <SideScrollNewsSection category={"hot"} />
-        <RectAds
-          type={
-            "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-          }
-        />
-        <div className="mb-10 pb-4">
-          {/* middle scrolling news under the hero section */}
-          <EachCategoryPreview category="health" />
-          <RectAds
-            type={
-              "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-            }
-          />
-        </div>
-        <div className="mb-10 pb-4">
-          {/* middle scrolling news under the hero section */}
-          <EachCategoryPreview category="technology" />
-          <RectAds
-            type={
-              "ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
-            }
-          />
-        </div>
-        {/* other category previews go here */}
-        {/* I think other category previews should be placed here ok */}
-        {/* end of categories preview / middle scrolling section ends here */}
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div className=" w-full px-4">
+				{hot && (
+					<div className="flex gap-6">
+						<div className="w-full sm:w-2/3">
+							<HomeHero data={hot[0]} />
+						</div>
+						<div className="w-1/3 sm:block hidden">
+							<h2 className=" text-2xl font-semibold leading-loose">
+								Recent News
+							</h2>
+							<RecentNews data={hot.slice(1)} />
+						</div>
+					</div>
+				)}
+				<h1 className="font-semibold text-2xl">Hottest Topics</h1>
+				<SideScrollNewsSection category={"hot"} />
+				<RectAds
+					type={
+						"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+					}
+				/>
+				<div className="mb-10 pb-4">
+					{/* middle scrolling news under the hero section */}
+					<EachCategoryPreview category="health" />
+					<RectAds
+						type={
+							"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+						}
+					/>
+				</div>
+				<div className="mb-10 pb-4">
+					{/* middle scrolling news under the hero section */}
+					<EachCategoryPreview category="technology" />
+					<RectAds
+						type={
+							"ma X category sita relevent ad ho, Life Insurance garnuhos Y life insurance"
+						}
+					/>
+				</div>
+				{/* other category previews go here */}
+				{/* I think other category previews should be placed here ok */}
+				{/* end of categories preview / middle scrolling section ends here */}
+			</div>
+		</>
+	)
+}
 
-export default Landing;
+export default Landing
