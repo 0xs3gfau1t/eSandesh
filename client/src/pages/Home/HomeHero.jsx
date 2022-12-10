c9c35f2ac02792cf37201c5d3bc36286e27d10fa
SandeshGC <gcsandesh01@gmail.com>
2022-12-10 22:25:45 +0545

    add links to preview images

--- client/src/pages/Home/HomeHero.jsx
+++ client/src/pages/Home/HomeHero.jsx
@@ -1,49 +1,52 @@
-import React from "react"
+import React from "react";
 
-import { Link } from "react-router-dom"
-import { LikeSaveShare } from "../../components/common"
+import { Link } from "react-router-dom";
+import { LikeSaveShare } from "../../components/common";
 
 export default function HomeHero({ data }) {
-	const div = document.createElement("div")
-	div.innerHTML = data ? data.content : ""
-	const img = div.querySelector("img")
+  const div = document.createElement("div");
+  div.innerHTML = data ? data.content : "";
+  const img = div.querySelector("img");
 
-	return (
-		<div className="mb-10 pb-4">
-			<h1 className="text-5xl font-primary font-bold leading-loose">
-				{/* heading */}
-				Today's News
-			</h1>
-			<p className="w-full">
-				{/* cover Img */}
-				<img
-					className="w-full"
-					src={
-						img
-							? img.src
-							: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-					}
-				/>
-			</p>
-			{/* below cover img */}
-			<div>
-				<h1 className="text-3xl my-2">
-					<Link to={""} className="hover:text-rose-600 duration-300">
-						{/* article title */}
-						{data.title}
-					</Link>
-				</h1>
-				<div className="flex justify-between">
-					<div>
-						{/* short summary of article */}
-						Summary short
-					</div>
-					<LikeSaveShare
-						// articleId = {someArticleId}
-						likes={"1.2k"}
-					/>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="mb-10 pb-4">
+      <h1 className="text-5xl font-primary font-bold leading-loose">
+        {/* heading */}
+        Today's News
+      </h1>
+      <p className="w-full">
+        {/* cover Img */}
+        <Link to={""}>
+          {/*link to articleURL*/}
+          <img
+            className="w-full"
+            src={
+              img
+                ? img.src
+                : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+            }
+          />
+        </Link>
+      </p>
+      {/* below cover img */}
+      <div>
+        <h1 className="text-3xl my-2">
+          <Link to={""} className="hover:text-rose-600 duration-300">
+            {/* article title */}
+            {data.title}
+          </Link>
+        </h1>
+        <div className="flex justify-between">
+          <div>
+            {/* short summary of article */}
+            Summary short
+          </div>
+          <LikeSaveShare
+            // articleId = {someArticleId}
+            likes={"1.2k"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
