6e6122312774902ed3e499dc355d0d779ec10f9a
paraspoudel72 <pas076bct020@wrc.edu.np>
2022-12-12 00:33:40 +0545

    clean code

--- client/src/redux/actions/comments.js
+++ client/src/redux/actions/comments.js
@@ -4,13 +4,12 @@ import axios from "axios"
 export const addComments = createAsyncThunk(
 	"news/addComments",
 	async (comment, { dispatch }) => {
-		// console.log("data: ", data)
+		console.log("data add gareko: ", comment)
 		const response = await axios
 			.post("/api/comment", comment, {
 				withCredentials: true,
 			})
 			.then(res => {
-                console.log("add vayo hola")
 				return res.data
 			})
 			.catch(err => {
@@ -26,21 +25,17 @@ export const addComments = createAsyncThunk(
 export const listCommentsByArticle = createAsyncThunk(
 	"news/listCommentsByArticle",
 	async (params, { dispatch }) => {
-        console.log(params)
         const page = 0, items = 10;
-		// const {articleId, page, items} = params;
 		const response = await axios
 			.get(`/api/comment?articleId=${params}&page=${page}&items=${items}`, {
 				withCredentials: true,
 			})
 			.then(res => {
-                // console.log(res.data)
-                console.log('milyo')
+                console.log(res.data)
 				return res.data
 			})
 			.catch(err => {
 				console.error(err)
-                console.log("bigryo")
 			})
 		if (!response) return { success: false }
 		return { success: true, data: response, page: page }
