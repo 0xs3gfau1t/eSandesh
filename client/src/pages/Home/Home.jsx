a1c11995fbf76666459bc7bae0495bfaa335bbd9
SandeshGC <gcsandesh01@gmail.com>
2022-12-11 19:18:37 +0545

    fix merge conflict in Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import { HiArrowNarrowRight } from "react-icons/hi"
-import { Link } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { Outlet } from "react-router-dom"
-=======
 import React from "react";
 // import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
->>>>>>> 612a846906c30647d5423916fead6849188beff7
 
 import { Footer, Header, LeftSideBar, RightSideBar } from "../../components";
 
-<<<<<<< HEAD
-export default function Home() {
-	const focus = useSelector(state => state.misc.focus)
-=======
 export default function Home(match) {
   const focus = useSelector((state) => state.misc.focus);
->>>>>>> 612a846906c30647d5423916fead6849188beff7
 
   return (
     <div className="flex flex-col h-screen">
