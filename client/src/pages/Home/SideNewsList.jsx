1809e4b559d13e615753f0d928f833d9d9975034
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-12 22:02:07 +0545

    Add stories section in landing page

--- client/src/pages/Home/SideNewsList.jsx
+++ client/src/pages/Home/SideNewsList.jsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import { FaAngleDoubleRight } from "react-icons/fa";
+import { FaAngleDoubleRight } from "react-icons/fa"
 
-export default function SideNewsList({ category }) {
-  return (
-    <ul className="py-2">
-      <li className="flex items-start">
-        <FaAngleDoubleRight className="mr-2 mt-1" />
-        <span className="hover:text-rose-700 font-semibold transition-colors">
-          <Link to={"articleURL"}>
-            Walter White gets Cancer, from Staying in the sun({category})
-          </Link>
-        </span>
-      </li>
-      <li className="flex items-center">
-        <FaAngleDoubleRight className="mr-2" />
-        <span className="hover:text-rose-700 font-semibold transition-colors">
-          <Link to={"articleURL"}>Drink Warm Water({category})</Link>
-        </span>
-      </li>
-    </ul>
-  );
+export default function SideNewsList({ category, data }) {
+	if (data.length > 0)
+		return (
+			<ul className="py-2">
+				{data.map((news, index) => {
+					return (
+						<li className="flex items-start">
+							<FaAngleDoubleRight className="mr-2 mt-1" />
+							<span className="hover:text-rose-700 font-semibold transition-colors">
+								<Link
+									to={`news/${news.year}/${news.month}/${news.slug}`}
+								>
+									{news.title}
+								</Link>
+							</span>
+						</li>
+					)
+				})}
+			</ul>
+		)
+	else return <h1>No more news on {category}</h1>
 }
