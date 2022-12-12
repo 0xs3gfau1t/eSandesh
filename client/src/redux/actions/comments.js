6c3984ba7fb40efcd0e07b56d83fd6f91002cd41
paraspoudel72 <pas076bct020@wrc.edu.np>
2022-12-12 19:12:41 +0545

    dlt feature implement

--- client/src/redux/actions/comments.js
+++ client/src/redux/actions/comments.js
@@ -1,10 +1,35 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 
+
+export const dltComments = createAsyncThunk(
+	"news/dltComments",
+	async (comment, { dispatch }) => {
+		const response = await axios
+			.delete("/api/comment", {
+				withCredentials: true,
+				data: { commentId: comment.commentId }
+			})
+			.then(res => {
+				console.log(res.data)
+				return res.data
+			})
+			.catch(err => {
+				console.error(err)
+			})
+
+		if (!response) return { success: false }
+
+		return { success: true }
+	}
+)
+
+
+
+
 export const addComments = createAsyncThunk(
 	"news/addComments",
 	async (comment, { dispatch }) => {
-		console.log("data add gareko: ", comment)
 		const response = await axios
 			.post("/api/comment", comment, {
 				withCredentials: true,
@@ -22,10 +47,58 @@ export const addComments = createAsyncThunk(
 	}
 )
 
+// export const editComments = createAsyncThunk(
+// 	"news/editComments",
+// 	async (comment, { dispatch }) => {
+// 		const response = await axios
+// 			.patch("/api/comment",comment, {
+// 				withCredentials: true,
+// 				data: { commentId: comment.commentId, content: comment.content }
+// 			})
+// 			.then(res => {
+// 				return res.data
+// 			})
+// 			.catch(err => {
+// 				console.error(err)
+// 			})
+
+// 		if (!response) return { success: false }
+
+// 		return { success: true }
+// 	}
+// )  
+
+
+
+
+
+
+export const addSubComments = createAsyncThunk(
+	"news/addSubComments",
+	async (comment, { dispatch }) => {
+		const response = await axios
+			.post("/api/comment", comment, {
+				withCredentials: true,
+			})
+			.then(res => {
+				return res.data
+			})
+			.catch(err => {
+				console.error(err)
+			})
+
+		if (!response) return { success: false }
+
+		return { success: true }
+	}
+)
+
+
+
 export const listCommentsByArticle = createAsyncThunk(
 	"news/listCommentsByArticle",
 	async (params, { dispatch }) => {
-        const page = 0, items = 10;
+        const page = 0, items = 40;
 		const response = await axios
 			.get(`/api/comment?articleId=${params}&page=${page}&items=${items}`, {
 				withCredentials: true,
