38c1b26c2f343ea5fa0b72536951a8c360ecb00a
paraspoudel72 <pas076bct020@wrc.edu.np>
2022-12-12 22:27:49 +0545

    need merge

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -111,7 +111,7 @@ function Comments(props) {
                     )
                 }  
             })}
-            <div>
+            <div className='w-full'>
                 <form onSubmit={handleSubmit}>
                     <input className="placeholder:italic placeholder:text-slate-400 bg-white w-64 border border-slate-300 rounded-md py-2  px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type = "text" value = {value.txtvalue} onChange = {handleChange} placeholder = "Type your Comment"/>
                     <input className="w-32 text-sm cursor-pointer text-slate-900  py-2 px-2 rounded-full border-0 font-semibold transition-all duration-200 hover:bg-orange-500" type="submit" value="Submit" />
