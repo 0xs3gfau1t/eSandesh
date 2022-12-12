37e4536b4740d1e75e360f6eeb250683dcc2045e
SandeshGC <gcsandesh01@gmail.com>
2022-12-12 23:32:24 +0545

    added navigation for user dashboard

--- client/src/pages/Home/Dashboard.jsx
+++ client/src/pages/Home/Dashboard.jsx
@@ -1,27 +1,42 @@
-import { useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { useSession } from "next-auth/react"
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useSession } from "next-auth/react";
 
-import { setFocus } from "../../redux/reducers/misc"
+
+import { setFocus } from "../../redux/reducers/misc";
+import UserDashNav from "./UserDash/UserDashNav";
 
 const DashBoard = () => {
-	const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
 
-	const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(setFocus(true))
-	}, [])
+  useEffect(() => {
+    dispatch(setFocus(true));
+  }, []);
 
-	if (status == "unauthenticated") {
-		return <h1>You must login to view this page</h1>
-	}
+  if (status == "unauthenticated") {
+    return (
+      <h1 className="text-3xl font-bold">You must login to view this page</h1>
+    );
+  }
 
-	return (
-		<div>
-			<h1>I am user ko Dashboard.</h1>
+  return (
+    <div className="w-full">
+		<div className="flex items-start w-full border-4 border-red">
+		 <UserDashNav/>
+
+		  {/* end of left menu col */}
+		  <div className="border-4 border-black flex">
+			<div>News 1</div>
+			<div>Little Preview</div>
+			<div>Created/modified date</div>
+			<div>edit btn</div>
+			<div>delete btn</div>
+		  </div>
 		</div>
-	)
-}
+	</div>
+  );
+};
 
-export default DashBoard
+export default DashBoard;
