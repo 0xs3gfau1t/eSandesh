a8efca1e9401694eb8a17fb15147b8d3bb07dfb5
bimarshak7 <khanalbimarsha7@gmail.com>
2022-12-13 19:31:17 +0545

    Create polls page for users

--- client/src/redux/reducers/polls.js
+++ client/src/redux/reducers/polls.js
@@ -1,25 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { createPoll, listPolls } from "../actions/polls";
+import { createSlice } from "@reduxjs/toolkit"
+import { createPoll, listPolls, votePoll } from "../actions/polls"
 
-const initialState = { pollsList: [] };
+const initialState = { pollsList: [] }
 
 const pollsSlice = createSlice({
-  name: "polls",
-  initialState,
-  reducers: {},
+	name: "polls",
+	initialState,
+	reducers: {},
 
-  extraReducers: (builder) => {
-    builder.addCase(listPolls.fulfilled, (state, { payload }) => {
-      if (payload.success && payload.data) {
-        state.pollsList = payload.data;
-      }
-    });
-    builder.addCase(createPoll.fulfilled, (state, { payload }) => {
-      if (payload.success && payload.data) {
-        state.pollsList.unshift(payload.data.poll);
-      }
-    });
-  },
-});
+	extraReducers: builder => {
+		builder.addCase(listPolls.fulfilled, (state, { payload }) => {
+			if (payload.success && payload.data) {
+				payload.data.map(function (entry) {
+					entry.casted = false
+				})
+				state.pollsList = payload.data
+			}
+		})
+		builder.addCase(createPoll.fulfilled, (state, { payload }) => {
+			if (payload.success && payload.data) {
+				state.pollsList.unshift(payload.data.poll)
+			}
+		})
+		builder.addCase(votePoll.fulfilled, (state, { payload }) => {
+			if (payload.success && payload.data) {
+				state.pollsList[payload.data.index].casted = true
+			}
+		})
+	},
+})
 
-export default pollsSlice.reducer;
+export default pollsSlice.reducer
