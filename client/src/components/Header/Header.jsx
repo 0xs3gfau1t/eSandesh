f6e8e4918cf24de710528db2ffb20e9119a6d964
SandeshGC <gcsandesh01@gmail.com>
2022-12-13 23:32:14 +0545

    modify user options in header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -31,10 +31,11 @@ export default function Header() {
 						onClick={(e) => setShow(!show)}
 					/>
 					{show && (
-						<ul className="header-drop absolute z-99 right-0 mt-32 p-2 w-28 text-center rounded-lg">
+						<ul className="header-drop absolute z-99 right-0 mt-32 p-2 w-36 text-center border-blue border-2 rounded-md font-bold text-xl">
 							<Link to="/profile">
-								<li>Profile</li>
+								<li>प्रोफाइल</li>
 							</Link>
+							<hr className="w-11/12 border-neutral-300"/>
 							<li onClick={action}>
 								{session.status == "authenticated" ? "लग आउट" : "लग इन"}
 							</li>
