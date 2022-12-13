b477021bf1fda18da9314af885ce125b0235a3dd
SandeshGC <gcsandesh01@gmail.com>
2022-12-14 00:02:19 +0545

    translate footer

--- client/src/components/Footer/SocialIcons.jsx
+++ client/src/components/Footer/SocialIcons.jsx
@@ -5,17 +5,17 @@ export default function SocialIcons() {
 	return (
 		<div className="flex gap-3 items-cente">
 			<a href="https://www.facebook.com" target={"_blank"}>
-				<span>
+				<span className="hover:text-[#4267B2] duration-200 ">
 					<FaFacebookF className="text-2xl" />
 				</span>
 			</a>
 			<a href="https://www.twitter.com" target={"_blank"}>
-				<span>
+				<span className="hover:text-[#1DA1F2] duration-200 ">
 					<FaTwitter className="text-2xl" />
 				</span>
 			</a>
 			<a href="https://www.youtube.com" target={"_blank"}>
-				<span>
+				<span className="hover:text-[#FF0000] duration-200 ">
 					<FaYoutube className="text-2xl" />
 				</span>
 			</a>
