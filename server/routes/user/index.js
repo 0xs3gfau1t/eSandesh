84310e0f8607115496e287e9a4599716b25faff6
needl3 <57314527+needl3@users.noreply.github.com>
2022-12-13 23:27:01 +0545

    Update topuser route inside user

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -6,5 +6,6 @@ const withAuth = require('../../middlewares/withAuth')
 router.get('/article', withAuth, require('./getSavedArticles'))
 router.post('/article', withAuth, require('./saveArticle'))
 router.delete('/article', withAuth, require('./removeSavedArticle'))
+router.get('/topuser', withAuth, require('./topUser'))
 
 module.exports = router
--- server/routes/user/topUser.js
+++ server/routes/user/topUser.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const commentModel = require('../model/comment')
+const commentModel = require('../../model/comment')
 
 /**
  * @param {express.Request} req
