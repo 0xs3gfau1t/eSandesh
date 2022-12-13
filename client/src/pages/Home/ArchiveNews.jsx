4a005132ebdce61ebfe9b213a3ddb90607f93701
paraspoudel72 <pas076bct020@wrc.edu.np>
2022-12-13 18:28:34 +0545

    add card to page

--- client/src/pages/Home/ArchiveNews.jsx
+++ client/src/pages/Home/ArchiveNews.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { FaUserCircle, FaFilter } from "react-icons/fa";
+import ArchiveCard from '../../components/common/ArchiveCard';
 
 function ArchiveNews() {
   return (
-    <div>ArchiveNews</div>
+    <div>
+      <div>Filter smth smth</div>
+      <ArchiveCard/>
+    
+    </div>
+
+
+
+
+    
   )
 }
 
