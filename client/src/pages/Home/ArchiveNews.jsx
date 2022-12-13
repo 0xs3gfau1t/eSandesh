adfa562d588b2cc5dea3b59701e64eb4a7ecd587
paraspoudel72 <pas076bct020@wrc.edu.np>
2022-12-13 18:59:36 +0545

    filter icon

--- client/src/pages/Home/ArchiveNews.jsx
+++ client/src/pages/Home/ArchiveNews.jsx
@@ -6,15 +6,23 @@ import ArchiveCard from '../../components/common/ArchiveCard';
 function ArchiveNews() {
   return (
     <div>
-      <div>Filter smth smth</div>
+      {/* <input type="dropdown" onClick><FaFilter/></input> */}
+      <div className='flex gap-2 items-center'>
+        <FaFilter/>
+        <select id="cars" name="cars">
+          <option value="date">By NewsDate</option>
+          <option value="category">By Category</option>
+          <option value="archived date">By Archived Date</option>
+        </select>
+      </div>
+     
       <ArchiveCard/>
-    
+      <ArchiveCard/>
+      <ArchiveCard/>
+      <ArchiveCard/>
+      <ArchiveCard/>
+      <ArchiveCard/>
     </div>
-
-
-
-
-    
   )
 }
 
