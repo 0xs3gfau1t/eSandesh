924f3417174a40b20355ec9189e1f8a564a54f92
SandeshGC <gcsandesh01@gmail.com>
2022-12-14 09:26:36 +0545

    fix social media icons

--- client/src/components/common/SocialShare.jsx
+++ client/src/components/common/SocialShare.jsx
@@ -15,48 +15,56 @@ import { BsFillSaveFill } from "react-icons/bs"
 import { useDispatch } from "react-redux"
 
 import { saveNews } from "../../redux/actions/publicNews"
+import { FaLink } from "react-icons/fa"
 
 const SocialShare = ({ title, id }) => {
 	const url = document.location.href
 	const dispatch = useDispatch()
 
-	const saveSave = e => {
+	const saveSave = (e) => {
 		// console.log(id)
 		dispatch(saveNews({ id: id }))
 	}
 
 	return (
-		<div className="flex ">
-			<h1 className="font-bold py-3">Share this news</h1>
-			<FacebookShareButton
-				quote={title}
-				url={url}
-				hashtag={["#esandesh"]}
-				className="social-share"
-			>
-				<FacebookIcon size={28} />
-			</FacebookShareButton>
-
-			<TwitterShareButton
-				title={title}
-				url={url}
-				hashtags={["eSandesh"]}
-				className="social-share"
-			>
-				<TwitterIcon size={28} />
-			</TwitterShareButton>
-
-			<RedditShareButton title={title} className="social-share">
-				<RedditIcon size={28} />
-			</RedditShareButton>
-
-			<LineShareButton title={title} className="social-share">
-				<LinkedinIcon size={28} />
-			</LineShareButton>
-
-			<BsFillSaveFill
-				className="social-share text-3xl ml-4 my-3 text-darkblue cursor-pointer"
-				onClick={saveSave}
+		<div className="flex gap-2 items-center">
+			<p className="font-bold">यो खबर सेयर गर्नुहोस्:</p>
+			<div className="flex items-center">
+				{/* group of social media icons */}
+				<div title={"Post to Facebook"}>
+					<FacebookShareButton
+						quote={title}
+						url={url}
+						hashtag={["#esandesh"]}
+						className="social-share"
+					>
+						<FacebookIcon size={28} />
+					</FacebookShareButton>
+				</div>
+				<div title={"Tweet this article"}>
+					<TwitterShareButton
+						url={url}
+						hashtags={["eSandesh"]}
+						className="social-share"
+					>
+						<TwitterIcon size={28} />
+					</TwitterShareButton>
+				</div>
+				<div title={"Share to Reddit"}>
+					<RedditShareButton className="social-share">
+						<RedditIcon size={28} />
+					</RedditShareButton>
+				</div>
+				<div title="Share to LinkedIn">
+					<LineShareButton className="social-share">
+						<LinkedinIcon size={28} />
+					</LineShareButton>
+				</div>
+			</div>
+			{/* share by link */}
+			<FaLink
+				className="my-auto w-6 h-6 cursor-pointer hover:text-blue duration-200"
+				title="Copy Link"
 			/>
 		</div>
 	)
