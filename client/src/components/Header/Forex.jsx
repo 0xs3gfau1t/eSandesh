d3304402dc0bab469dabd4c4d5abd12b8c64a233
paraspoudel72 <pas076bct020@wrc.edu.np>
2023-02-11 23:38:46 +0545

    implement on redux

--- client/src/components/Header/Forex.jsx
+++ client/src/components/Header/Forex.jsx
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { updateForexData } from "../../redux/reducers/misc";
+
 
 export default function Forex() {
-	const [forexData, setForexData] = useState([]);
+	// const [forexData, setForexData] = useState([]);
+	const forexData = useSelector(state =>state.misc.data)
+	// const focus = useSelector((state) => state.misc.focus)
+	const dispatch = useDispatch();
 
-
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const response = await fetch("https://www.nrb.org.np/api/forex/v1/rate");
+	// 		const data = await response.json();
+	// 		setForexData(data.data.payload.rates);
+	// 	};
+	// 	fetchData();
+	//   }, []);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			const response = await fetch("https://www.nrb.org.np/api/forex/v1/rate");
-			const data = await response.json();
-			setForexData(data.data.payload.rates);
+			const response_data = await response.json();
+			const data = response_data.data.payload.rates;
+			dispatch(updateForexData(data))
 		};
 		fetchData();
-	  }, []);
+	  }, [dispatch]);
 
-
-
+	  
 	  return (
 		<div className=" grid table-fixed w-8/12 ">
 		  <div className=" overflow-x-auto ">
 			<table className="">
 			  <thead>
 				<tr>
-				  <th className="px-4 py-2">Currency</th>
+				  <th className="px-4 py-2">Currency</th>{console.log(forexData)}
 				  {forexData.map(value => (
 					<th key={value.currency.iso3} className="px-4 py-2">
 					  {value.currency.iso3}
@@ -32,7 +45,7 @@ export default function Forex() {
 			  </thead>
 			  <tbody>
 				<tr>
-				  <td className="px-4 py-2">Buy</td>
+				  <td className="px-4 py-2 font-bold">Buy</td>
 				  {forexData.map(value => (
 					<td key={value.currency.iso3} className="px-4 py-2">
 					  {value.buy}
@@ -40,7 +53,7 @@ export default function Forex() {
 				  ))}
 				</tr>
 				<tr>
-				  <td className="px-4 py-2">Sell</td>
+				  <td className="px-4 py-2 font-bold">Sell</td>
 				  {forexData.map(value => (
 					<td key={value.currency.iso3} className="px-4 py-2">
 					  {value.sell}
