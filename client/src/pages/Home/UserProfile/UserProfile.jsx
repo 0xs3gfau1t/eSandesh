9909608ca38a86abd65910dba72de6f0c4354a5f
paraspoudel72 <pas076bct020@wrc.edu.np>
2023-02-13 19:31:19 +0545

    popup for user profile

--- client/src/pages/Home/UserProfile/UserProfile.jsx
+++ client/src/pages/Home/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useSession } from "next-auth/react"
 
@@ -11,6 +11,7 @@ import UserPosts from "./UserPosts"
 
 const UserProfile = () => {
 	const { data: session, status } = useSession()
+	const [showProfile, setShowProfile] = useState(false);
 
 	const dispatch = useDispatch()
 
@@ -26,12 +27,31 @@ const UserProfile = () => {
 
 	return (
 		<div className="font-english p-4 flex flex-col ">
-			<h1 className="font-english underline underline-offset-2 leading-loose font-bold text-center text-3xl text-darkblue">
-				User Profile
-			</h1>
-			<UserInfo />
 			<hr className=" w-11/12 my-6 border-neutral-300" />
-			<UserPreference />
+			<div className="flex justify-between items-center">
+				<UserPreference />
+				<button className=" bg-primary w-64 p-2 rounded"
+						onClick ={()=> setShowProfile(!showProfile)}>
+					Set Your User Profile
+				</button>
+				{showProfile && (
+					<div className="fixed content-end">
+						<div className="m-auto p-4 ince bg-white rounded shadow-lg">
+							<h1 className="font-english underline underline-offset-2 leading-loose font-bold text-center text-3xl text-darkblue">
+								User Profile
+							</h1>
+							<UserInfo />
+							<hr className=" w-11/12 my-6 border-neutral-300" />
+							<button
+							className="text-red p-2 rounded hover:text-white hover:bg-red duration-200"
+							onClick={() => setShowProfile(false)}
+							>
+							Close
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
 			<hr className=" w-11/12 my-6 border-neutral-300" />
 			<UserPosts />
 			<hr className=" w-11/12 my-6 border-neutral-300" />
