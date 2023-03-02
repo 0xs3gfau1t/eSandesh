492d77457aad05578f5187c436a0a2eff53d4197
needl3 <57314527+needl3@users.noreply.github.com>
2023-03-02 13:39:33 +0545

    Ad image serving route
    - Ad images now can be directly downloaded/loaded by GETting
      /api/ads/images/:id
    - Filename sanitization for the same route
    - Adding extension on uploaded filenames

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 const { randomUUID } = require('crypto')
 
+function getExt(resource) {
+    console.log('resource: ', resource)
+    return '.' + resource.mimetype.split('/')[1]
+}
+
 module.exports = function uploadAdAssets(imageX, imageY, imageSq, audio) {
     //
-    // TODO: Sanitize and use filename instead of random uuid
     // Provide custom random uuid instead of using name
     // Because name has to be sanitized for path injection vulnerability counter measures
+    // Intentionally haven't used 'sanitize-filename' to be safe
     //
     let imageXUrl = undefined
     let imageYUrl = undefined
@@ -13,19 +18,19 @@ module.exports = function uploadAdAssets(imageX, imageY, imageSq, audio) {
     let audioUrl = undefined
 
     if (imageX) {
-        imageXUrl = randomUUID()
+        imageXUrl = randomUUID() + getExt(imageX)
         fs.writeFileSync('./assets/ads/images/' + imageXUrl, imageX.data)
     }
     if (imageY) {
-        imageYUrl = randomUUID()
+        imageYUrl = randomUUID() + getExt(imageY)
         fs.writeFileSync('./assets/ads/images/' + imageYUrl, imageY.data)
     }
     if (imageSq) {
-        imageSqUrl = randomUUID()
+        imageSqUrl = randomUUID() + getExt(imageSq)
         fs.writeFileSync('./assets/ads/images/' + imageSqUrl, imageSq.data)
     }
     if (audio) {
-        audioUrl = randomUUID()
+        audioUrl = randomUUID() + getExt(audio)
         fs.writeFileSync('./assets/ads/audio/' + audioUrl, audio.data)
     }
     return [
