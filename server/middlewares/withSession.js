24991399ea1127bc14247a6cb0f9124678ab6e2e
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-03-04 00:26:48 +0545

    Bump up next auth version

--- server/middlewares/withSession.js
+++ server/middlewares/withSession.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { unstable_getServerSession } = require('next-auth/next')
+const { getServerSession } = require('next-auth/next')
 const { authOptions } = require('../config/authOptions')
 
 /**
@@ -7,15 +7,7 @@ const { authOptions } = require('../config/authOptions')
  * @param {express.Response} res
  */
 const withSession = async (req, res, next) => {
-    // Donot directly pass the authOptions
-    // Doing so will reset the providers
-    // Idk why but it does. Only way to fix
-    // this for now is duplicate the object
-    // and then pass it.
-
-    const session = await unstable_getServerSession(req, res, {
-        ...authOptions,
-    })
+    const session = await getServerSession(req, res, authOptions)
 
     req.session = session
     return next()
