6bc7c33c73ff016bdb9585d4d53744dd5a7a8a1f
needl3 <57314527+needl3@users.noreply.github.com>
2023-03-12 22:50:08 +0545

    Route to approve eligible critics

--- server/routes/critics/index.js
+++ server/routes/critics/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const withPublisher = require('@/middlewares/withPublisher')
+
 router.get('/list', require('./listCritic'))
 router.get('/', require('./getCritic'))
-router.post('/', require('./addCritic'))
-router.patch('/', require('./editCritic'))
-router.delete('/', require('./deleteCritic'))
+router.post('/', withPublisher, require('./addCritic'))
+router.patch('/', withPublisher, require('./editCritic'))
+router.delete('/', withPublisher, require('./deleteCritic'))
+router.post('/approve', withPublisher, require('./approveCritic'))
 
 router.get('*', (_req, res) => {
     res.json({ message: 'yamete kudasai, critic san!' })
