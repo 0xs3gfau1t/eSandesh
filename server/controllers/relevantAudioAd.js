ab46f77981c45507cf6e867b0e68b36449f54436
needl3 <57314527+needl3@users.noreply.github.com>
2023-05-07 11:39:37 +0545

    Diversified but quota fulfilling audioAds
    - Bug fixes
    - Facets in aggregation to make sure if filtered ads are not
      available, non filtered ads will be included to reduce the use of
    fallback ad

--- server/controllers/relevantAudioAd.js
+++ server/controllers/relevantAudioAd.js
@@ -7,68 +7,174 @@ async function getRelevantAudioAd(history) {
     const { categoryStrength } = calculateCategoryStrength(history)
     const selectedAd = await adsModel.aggregate([
         {
-            $match: {
-                audio: {
-                    $exists: true,
-                },
-                category: {
-                    $in: Object.keys(categoryStrength),
-                },
-            },
-        },
-        {
-            $sort: {
-                priority: -1,
-            },
-        },
-        {
-            $skip: 0 * 10,
-        },
-        {
-            $group: {
-                _id: '$category',
-                final: {
-                    $push: {
-                        id: '$_id',
-                        name: '$name',
-                        priority: '$priority',
-                        audio: '$audio',
+            $facet: {
+                withCategory: [
+                    {
+                        $match: {
+                            audio: {
+                                $exists: true,
+                            },
+                            category: {
+                                $in: Object.keys(categoryStrength),
+                            },
+                        },
                     },
-                },
-            },
-        },
-        {
-            $unwind: {
-                path: '$_id',
-            },
-        },
-        {
-            $unwind: {
-                path: '$final',
-            },
-        },
-        {
-            $group: {
-                _id: '$_id',
-                final: {
-                    $push: {
-                        id: '$final.id',
-                        name: '$final.name',
-                        priority: '$final.priority',
-                        audio: '$final.audio',
+                    {
+                        $project: {
+                            audio: 0,
+                            image: 0,
+                        },
                     },
+                    {
+                        $sort: {
+                            priority: -1,
+                        },
+                    },
+                    {
+                        $skip: 0 * 10,
+                    },
+                    {
+                        $group: {
+                            _id: '$category',
+                            final: {
+                                $push: {
+                                    id: '$_id',
+                                    name: '$name',
+                                    priority: '$priority',
+                                    audio: '$audio',
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $unwind: {
+                            path: '$_id',
+                        },
+                    },
+                    {
+                        $unwind: {
+                            path: '$final',
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: '$_id',
+                            final: {
+                                $push: {
+                                    _id: '$final.id',
+                                    name: '$final.name',
+                                    priority: '$final.priority',
+                                    audio: '$final.audio',
+                                },
+                            },
+                        },
+                    },
+                ],
+                withOutCategory: [
+                    {
+                        $match: {
+                            audio: {
+                                $exists: true,
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            audio: 0,
+                            image: 0,
+                        },
+                    },
+                    {
+                        $sort: {
+                            priority: -1,
+                        },
+                    },
+                    {
+                        $skip: 0 * 10,
+                    },
+                    {
+                        $group: {
+                            _id: '$category',
+                            final: {
+                                $push: {
+                                    id: '$_id',
+                                    name: '$name',
+                                    priority: '$priority',
+                                    audio: '$audio',
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $unwind: {
+                            path: '$_id',
+                        },
+                    },
+                    {
+                        $unwind: {
+                            path: '$final',
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: '$_id',
+                            final: {
+                                $push: {
+                                    _id: '$final.id',
+                                    name: '$final.name',
+                                    priority: '$final.priority',
+                                    audio: '$final.audio',
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $project: {
+                result: {
+                    $concatArrays: [
+                        '$withCategory',
+                        {
+                            $cond: {
+                                if: {
+                                    $eq: [{ $size: '$withCategory' }, 0],
+                                },
+                                then: '$withOutCategory',
+                                else: [],
+                            },
+                        },
+                    ],
                 },
             },
         },
+        { $unwind: '$result' },
+        { $replaceRoot: { newRoot: '$result' } },
     ])
 
+    let totalAvailableAds = 0
     const allocatedAds = selectedAd
         .map(ad => {
             const maxAllocated = Math.round(categoryStrength[ad._id] * AD_LIMIT)
             const sliced = ad.final.slice(0, maxAllocated)
+            totalAvailableAds += ad.final.length
             return sliced
         })
         .flat()
+    let freeSpace = AD_LIMIT - allocatedAds.length
+    while (freeSpace) {
+        // This variable will include ads in increasing order from final set
+        let selectedAdIndex = 0
+        // If relevant ads donot fill quota, travel through again and add
+        selectedAd.forEach(ad => {
+            if (freeSpace) {
+                allocatedAds.push(ad.final[selectedAdIndex])
+                freeSpace--
+            } else return
+        })
+        selectedAdIndex++
+    }
 
     return {
         begin: allocatedAds.at(0)?._id || 'ffffffffffffffffffffffff',
