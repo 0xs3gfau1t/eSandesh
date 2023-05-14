e579e2e299241e8c0ae67c69b408e376db2de8cc
needl3 <57314527+needl3@users.noreply.github.com>
2023-05-14 15:19:07 +0545

    Prepare express to serve statically built files for production

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis')
 
 const redisClient = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379,
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
 })
 
 module.exports = { redisClient }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('module-alias/register')
 const path = require('path')
+const express = require('express')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 const { redisClient } = require('@/config/redis')
@@ -20,7 +21,12 @@ const { app, server } = require('./config/app')
 app.use('/api', require('./middlewares/withSession'), require('./routes'))
 
 // Redirect everything other that /api/ to frontend
+if (process.env.NODE_ENV === 'production')
+    app.use(express.static(path.resolve(__dirname, './dist')))
+
 app.all('*', (_req, res) => {
+    if (process.env.NODE_ENV === 'production')
+        return res.sendFile(path.resolve(__dirname, './dist/index.html'))
     return res.status(404).json({ response: 'What are you doing onii-chaan?' })
 })
 
