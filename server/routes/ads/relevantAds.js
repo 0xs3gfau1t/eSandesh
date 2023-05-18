ee63a6087104f369d3ff1413b819590c9eed47df
needl3 <57314527+needl3@users.noreply.github.com>
2023-05-18 07:36:49 +0545

    Cold start populate ads

--- server/routes/ads/relevantAds.js
+++ server/routes/ads/relevantAds.js
@@ -99,13 +99,15 @@ async function relevantAds(req, res) {
     // This is used to match filter categories available in history
     // and of ads
     //
-    const aggregationMatchQuery = req.categoryStrength
-        ? {}
-        : {
-              _id: {
-                  $in: Object.keys(categoryStrength),
-              },
-          }
+    let aggregationMatchQuery = {}
+    if (req.stopRecursion !== true) {
+        aggregationMatchQuery = {
+            _id: {
+                $in: Object.keys(categoryStrength),
+            },
+        }
+    }
+
     const categoryAds = await adsModel.aggregate([
         {
             $match: matchQuery,
@@ -241,15 +243,17 @@ async function relevantAds(req, res) {
     // clear history recursively call this function
     // and forward response from there
     //
-    if (
-        categoryAds.length <= 0 &&
-        Object.keys(req.cookies.user.history).length > 0
-    ) {
+    if (categoryAds.length <= 0 && req?.stopRecursion !== true) {
         req.cookies.user.history = {}
+        req.stopRecursion = true
         req.categoryStrength = categoryStrength
         relevantAds(req, res)
-    } else if (req?.blockResponse === true) return finalCategoryAds
-    else
+    } else if (req?.blockResponse === true) {
+        //
+        // This clause is used primarily by getArticle.js
+        // To just extract ads and not send respone
+        return finalCategoryAds
+    } else
         res.json({
             message: 'success',
             ads: finalCategoryAds,
